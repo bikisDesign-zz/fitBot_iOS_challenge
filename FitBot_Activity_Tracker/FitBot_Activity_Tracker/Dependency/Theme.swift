c8b9ef8caf01ba28c587fcc1d232d381180c8df0
Aaron Bikis <aaron.bikis@gmail.com>
2018-04-07 13:24:30 -0700

    add theme and add button asset

---
+++ FitBot_Activity_Tracker/FitBot_Activity_Tracker/Dependency/Theme.swift
@@ -0,0 +1,22 @@
+//
+//  Theme.swift
+//  FitBot_Activity_Tracker
+//
+//  Created by Aaron bikis on 4/7/18.
+//  Copyright © 2018 Aaron bikis. All rights reserved.
+//
+
+import UIKit
+
+struct Theme {
+  enum Colors {
+    case fitBotRed
+    
+    var color: UIColor {
+      switch self {
+      case .fitBotRed:
+        return 
+      }
+    }
+  }
+}
