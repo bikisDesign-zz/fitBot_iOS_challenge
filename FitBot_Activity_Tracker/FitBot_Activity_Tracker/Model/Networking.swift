4b16cc21856032212d9ad0406e36f86e88aa61ed
Aaron Bikis <aaron.bikis@gmail.com>
2018-04-10 18:05:19 -0700

    add strava credentials

---
+++ FitBot_Activity_Tracker/FitBot_Activity_Tracker/Model/Networking.swift
@@ -0,0 +1,16 @@
+//
+//  Networking.swift
+//  FitBot_Activity_Tracker
+//
+//  Created by Aaron bikis on 4/9/18.
+//  Copyright © 2018 Aaron bikis. All rights reserved.
+//
+
+import Alamofire
+
+
+private struct StravaAPICredentials {
+  static let secret: String = "fe2ba8fc513e93d3ba740a13e4795cd24f14f50b"
+  static let token: String = "d3366318b7178759282d4f0e76ce3d5aae4926e4"
+  static let clientId: String = "24730"
+}
